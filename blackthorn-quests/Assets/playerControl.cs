03b14d1d32713180c707c1a493e5e6bfb95f4bc2
CameronFranco <44078559+CameronFranco@users.noreply.github.com>
2018-11-04 22:50:35 -0500

    quests

---
+++ blackthorn-quests/Assets/playerControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerControl : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    public float speed;
+
+    public Text WinText;
+
+    private bool Q1;
+
+    private int qCount;
+
+    public float time;
+
+    void Start()
+    { 
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical * 0.1f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Q"))
+        {
+            qCount++;
+            other.gameObject.SetActive(false);
+        }
+
+        if (other.gameObject. CompareTag("Finish"))
+        {
+            if (qCount == 3)
+            {
+                WinText.text = "You have finished! You completed the maze, crossed the sphere, and did not fly off the the ramp into neverending oblivion!!!";
+            }
+               
+        }
+
+
+    }
+}
---
+++ blackthorn-quests/Assets/player_control - Copy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player_control : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    public float speed;
+
+    public Text CountText;
+
+    int count;
+
+    public Text WinText;
+
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical * 0.1f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            count = count + 1;
+            other.gameObject.SetActive(false);
+            SetCountText();
+            if (count >= 14)
+            {
+                WinText.text = "You are victorious!";
+            }
+        }
+    }
+
+    void SetCountText()
+    {
+        CountText.text = "Count: " + count.ToString();
+    }
+   
+}
